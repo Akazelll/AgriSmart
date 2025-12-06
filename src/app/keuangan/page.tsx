e3f116976e1f861a87756a577919eef696f1a400
Adam Raga <adamxraga@gmail.com>
2025-12-06 19:36:41 +0700

    hps keuangan

--- src/app/keuangan/page.tsx
+++ src/app/keuangan/page.tsx
@@ -1,7 +1,6 @@
 export default function KeuanganPage() {
   return (
     <div className='flex min-h-screen w-full flex-col'>
-      <h1 className='text-2xl font-bold'>Keuangan</h1>
     </div>
   );
 }
