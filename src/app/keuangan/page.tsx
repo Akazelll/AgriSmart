51019867094fef8d7e1bdb62a598426ab9763151
Adam Raga <adamxraga@gmail.com>
2025-12-06 19:29:19 +0700

    add keuangan dikit

--- src/app/keuangan/page.tsx
+++ src/app/keuangan/page.tsx
@@ -1,7 +1,7 @@
 export default function KeuanganPage() {
-  return(
+  return (
     <div className='flex min-h-screen w-full flex-col'>
-
+      <h1 className='text-2xl font-bold'>Keuangan</h1>
     </div>
   );
 }
