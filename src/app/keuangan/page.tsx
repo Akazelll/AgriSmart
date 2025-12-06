629721130e0afd01fba7597ae1b5bf02811d6c6b
Adam Raga <adamxraga@gmail.com>
2025-12-06 19:38:37 +0700

    test keuangan

--- src/app/keuangan/page.tsx
+++ src/app/keuangan/page.tsx
@@ -1,6 +1,9 @@
 export default function KeuanganPage() {
   return (
     <div className='flex min-h-screen w-full flex-col'>
+      <h1 className='m-4 text-2xl font-bold'>
+        Halaman Keuangan
+      </h1>
     </div>
   );
 }
